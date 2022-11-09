1456668f1c9e37eac6a743f3cb1455836be96807
Balaji59321 <balaji59321@gmail.com>
2022-11-09 15:34:20 +0530

    Changed the axios endpoint

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://money-manager-backend-balaji.herokuapp.com",
 });
 
 export default instance;
